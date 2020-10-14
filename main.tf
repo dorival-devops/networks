40a2433a6a1c1bba48ec0715fa2663b31a4f68a5
Dorival <dorival.querino@gmail.com>
2020-10-14 20:03:38 -0300

    Commit inicial

---
+++ main.tf
@@ -0,0 +1,13 @@
+
+     resource "aws_vpc" "jlearning" {
+         cidr_block = "11.0.0.0/16"
+     }
+ 
+ resource "aws_subnet" "jlearning" {
+   vpc_id     = "${aws_vpc.jlearning.id}"
+   cidr_block = "11.0.1.0/24"
+ 
+   tags = {
+     projeto = "jlearning"
+   }
+ }
